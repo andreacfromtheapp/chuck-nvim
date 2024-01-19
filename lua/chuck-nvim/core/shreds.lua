dbcf3a8ff1b48e9ca6cdfafd8e5d539a7a6ff74a
gacallea <gacallea@users.noreply.github.com>
2024-01-19 19:36:12 +0100

    this is still not working but better logic to action here first

--- lua/chuck-nvim/core/shreds.lua
+++ lua/chuck-nvim/core/shreds.lua
@@ -9,63 +9,45 @@ local function column(line, n)
   return line:match(p)
 end
 
--- if the log line matches a certain word/pattern we set an action
-local function set_action(line)
-  local action
-  if string.match(line, "sporking incoming shred: %d") then
-    action = "add"
-  end
-  if string.match(line, "replacing shred %d") then
-    action = "replace"
-  end
-  if string.match(line, "removing shred: %d") then
-    action = "remove"
-  end
-  -- if clearing or exiting, empty the table and set action to nil
-  if string.match(line, "removing all shreds") then
-    for k in pairs(M.shreds_table) do
-      M.shreds_table[k] = nil
-    end
-    action = nil
-  end
-  return action
-end
-
 -- set the temporary table to raw ChucK data ["id"] = "value"
-function M.set_table(line)
-  local action = set_action(line)
-
-  -- if the log line matches a certain word/pattern
+function M.set_table(line, action)
   if action ~= nil then
-    local shred_id
-    local shred_name
-    -- get string length
-    local _, count = string.gsub(line, "% ", "")
-
-    -- iterate over the string
-    for n = 1, count + 1 do
-      -- split line to columns and match the last id and last filename
-      local id = column(line, n):match("%d+")
-      local name = column(line, n):match("%((.+)%).+")
-
-      -- if id is valid initialize shred_id
-      if id ~= nil then
-        shred_id = tostring(id)
+    -- if clearing or exiting, empty the table and set action to nil
+    if action == "clear" then
+      for k in pairs(M.shreds_table) do
+        M.shreds_table[k] = nil
       end
-      -- if name is valid initialize shred_name
-      if name ~= nil then
-        shred_name = name
-      end
+    else
+      local shred_id
+      local shred_name
+      -- get string length
+      local _, count = string.gsub(line, "% ", "")
 
-      -- if shred_id and shred_name are valid
-      if shred_id ~= nil and shred_name ~= nil and shred_name:match(".ck") then
-        -- if adding/replacing set shred as: id is key -> shred_name is value
-        if action == "add" or "replace" then
-          M.shreds_table[shred_id] = shred_name
+      -- iterate over the string
+      for n = 1, count + 1 do
+        -- split line to columns and match the last id and last filename
+        local id = column(line, n):match("%d+")
+        local name = column(line, n):match("%((.+)%).+")
+
+        -- if id is valid initialize shred_id
+        if id ~= nil then
+          shred_id = tostring(id)
         end
-        -- if removing set shred as nil: id is key -> nil
-        if action == "remove" then
-          M.shreds_table[shred_id] = nil
+        -- if name is valid initialize shred_name
+        if name ~= nil then
+          shred_name = name
+        end
+
+        -- if shred_id and shred_name are valid
+        if shred_id ~= nil and shred_name ~= nil and shred_name:match(".ck") then
+          -- if adding/replacing set shred as: id is key -> shred_name is value
+          if action == "add" or "replace" then
+            M.shreds_table[shred_id] = shred_name
+          end
+          -- if removing set shred as nil: id is key -> nil
+          if action == "remove" then
+            M.shreds_table[shred_id] = nil
+          end
         end
       end
     end
--- lua/chuck-nvim/core/utils.lua
+++ lua/chuck-nvim/core/utils.lua
@@ -4,18 +4,46 @@ local events = require("nui.utils.autocmd").event
 
 local M = {}
 
-local function shred_lines(logfile)
-  local file = assert(io.open(logfile, "r"))
-  file:seek("end")
-  local it = file:lines()
-  while vim.fn.getchar(0) == 0 do
-    local line = it()
-    if line then
-      shreds.set_table(line)
-      layout.shreds_tree:render()
+-- if the log line matches a certain word/pattern we set an action
+local function set_action(line)
+  local action
+  if line ~= nil then
+    if string.match(line, "sporking incoming shred: %d") then
+      action = "add"
+    end
+    if string.match(line, "replacing shred %d") then
+      action = "replace"
+    end
+    if string.match(line, "removing shred: %d") then
+      action = "remove"
+    end
+    if string.match(line, "removing all") then
+      action = "clear"
     end
   end
-  file:close()
+  return action
+end
+
+local function shred_set(line)
+  local action = set_action(line)
+  if line and action then
+    shreds.set_table(line, action)
+    layout.shreds_tree:render()
+  end
+end
+
+local function shred_lines(logfile)
+  local line = ""
+  vim.fn.jobstart({ "tail", "-f", tostring(logfile) }, {
+    on_stdout = function(_, data, _)
+      data[1] = line .. data[1]
+      line = data[#data]
+      data[#data] = nil
+      if #data > 0 then
+        shred_set(data[#data])
+      end
+    end,
+  })
 end
 
 local function start_chuck(cmd, logfile)

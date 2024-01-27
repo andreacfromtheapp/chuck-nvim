5f47f26eb384e9991d5551f4f4d95ac6e480df3d
gacallea <gacallea@users.noreply.github.com>
2024-01-27 14:29:39 +0100

    better sort when action is triggered

--- lua/chuck-nvim/core/shreds.lua
+++ lua/chuck-nvim/core/shreds.lua
@@ -17,15 +17,15 @@ function M.set_table(line, action)
       if shred_id ~= nil and shred_name ~= nil and shred_name:match(".ck") then
         if action == "add" or "replace" then
           M.shreds_table[shred_id] = { id = shred_id, name = shred_name, time = "00:00:00" }
+          -- make sure it's sorted by id in ascending order first
+          -- this doesn't seem to be working when adding shreds
+          table.sort(M.shreds_table, function(a, b)
+            return a.id < b.id
+          end)
         end
         if action == "remove" then
           M.shreds_table[shred_id] = nil
         end
-        -- make sure it's sorted by id in ascending order first
-        -- although this doesn't seem to be working when adding shreds live
-        table.sort(M.shreds_table, function(a, b)
-          return a.id < b.id
-        end)
       end
     end
   end

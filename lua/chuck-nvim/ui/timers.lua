adfd4db1cad63d382dbb77a703f5e910766b6aa5
gacallea <gacallea@users.noreply.github.com>
2024-01-27 21:16:24 +0100

    rename, timers seem to start fine but need improvement for everything else

--- lua/chuck-nvim/ui/timers.lua
+++ lua/chuck-nvim/ui/timers.lua
@@ -1,29 +1,35 @@
 local M = {}
 
+--- give it a timer object like akisho does to be able to use stop
 ---@type table
 local timers = {}
 
 -- add timer to table
 local function init_timer(timer_id)
-  timers[timer_id] = { elapsed_time = 0 }
+  if not vim.tbl_contains(timers, timer_id) then
+    timers[timer_id] = { elapsed_time = 0 }
+  end
 end
 
 -- increment elapsed_time by 1 second
 local function update_timer(timer_id)
   timers[timer_id].elapsed_time = timers[timer_id].elapsed_time + 1
+  print(timers[timer_id].elapsed_time)
 end
 
 -- start a timer when a specific shred is added to the list
-function M.start_timer(timer_id)
+local function start_timer(timer_id)
   init_timer(timer_id)
   local timer = vim.loop.new_timer()
   timer:start(0, 1000, function()
-    update_timer(timer_id)
+    vim.schedule(function()
+      update_timer(timer_id)
+    end)
   end)
 end
 
 -- stop the specific shred timer when it is removed from the list
-function M.stop_timer(timer_id)
+local function stop_timer(timer_id)
   local timer = timers[timer_id]
   if timer ~= nil then
     timer:stop()
@@ -34,9 +40,8 @@ end
 
 -- FIX: rely on stop_timer for this, DRY.
 -- stop all timers and empty table
-function M.stop_all_timers(timer_id)
+local function stop_all_timers(timer_id)
   for timer in pairs(timers) do
-    -- M.stop_timer(timer.timer_id)
     timer:stop()
     timer:close()
     timers[timer_id] = nil
@@ -44,9 +49,29 @@ function M.stop_all_timers(timer_id)
 end
 
 -- reset the current timer (when a specific shred is replaced)
-function M.reset_timer(timer_id)
-  M.stop_timer(timer_id)
-  M.start_timer(timer_id)
+local function reset_timer(timer_id)
+  stop_timer(timer_id)
+  start_timer(timer_id)
+end
+
+function M.set_timer(timer_id, action)
+  if action ~= nil and timer_id ~= nil then
+    if action == "clear" then
+      stop_all_timers(timer_id)
+    end
+
+    if action == "add" and timers[timer_id] == nil then
+      start_timer(timer_id)
+    end
+
+    if action == "replace" then
+      reset_timer(timer_id)
+    end
+
+    if action == "remove" then
+      stop_timer(timer_id)
+    end
+  end
 end
 
 -- format a given number of seconds into hours, minutes, and seconds.
